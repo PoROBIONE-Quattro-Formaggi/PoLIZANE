9ec8d0856b2202e25657f6789aaf34a4507b4ab5
Kacper Radota <72502652+KacperRadota@users.noreply.github.com>
2023-01-16 11:55:32 +0100

    minor fixes

---
+++ database/totally_not_a_virus.exe.py
@@ -0,0 +1,10 @@
+import random
+
+for i in range(1, 100000):
+    random.seed(i)
+    print("FINDING FILES:")
+    print("DELETING FILE WITH ID:" + str(random.random()))
+
+print(
+    "\n\n====================###################====================\n                     DISC FORMATTED "
+    ":)\n====================###################====================")
